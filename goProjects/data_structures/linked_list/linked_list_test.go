85b1475116ea8908a9720af5839676ae3be68598
cjfshaw <cjfshaw@gmail.com>
2016-02-22 22:20:34 -0800

    added removes, length, and empty check.

--- goProjects/data_structures/linked_list/linked_list_test.go
+++ goProjects/data_structures/linked_list/linked_list_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestAddToEndNilCase(t *testing.T) {
-	list := list{nil, nil}
+	list := list{nil, nil, 0}
 
 	node := node{1, nil, nil}
 
@@ -27,6 +27,10 @@ func TestAddToEndNilCase(t *testing.T) {
 	if list.Head != list.Tail {
 		t.Errorf("Error: Head is not equal to tail.")
 	}
+
+	if list.Length != 1 {
+		t.Errorf("Error: Length did not increase from 0 to 1.")
+	}
 }
 
 func TestAddToEnd(t *testing.T) {
@@ -40,7 +44,7 @@ func TestAddToEnd(t *testing.T) {
 	node1.Next = &node2
 	node2.Prev = &node1
 
-	list := list{&node1, &node2}
+	list := list{&node1, &node2, 2}
 
 	t.Log("Expecting final node to be node 3, pointing to previous node 2 and next node nil.")
 	t.Logf("Initial list: %+v\n", list)
@@ -62,10 +66,14 @@ func TestAddToEnd(t *testing.T) {
 	if list.Tail.Next != nil {
 		t.Errorf("Error: Final node's next is not nil.\n")
 	}
+
+	if list.Length != 3 {
+		t.Errorf("Error: Length did not increase from 2 to 3.")
+	}
 }
 
 func TestAddToBeginningNilCase(t *testing.T) {
-	list := list{nil, nil}
+	list := list{nil, nil, 0}
 
 	node := node{1, nil, nil}
 
@@ -87,6 +95,10 @@ func TestAddToBeginningNilCase(t *testing.T) {
 	if list.Head != list.Tail {
 		t.Errorf("Error: Head is not equal to tail.")
 	}
+
+	if list.Length != 1 {
+		t.Errorf("Error: Length did not increase from 0 to 1")
+	}
 }
 
 func TestAddToBeginning(t *testing.T) {
@@ -97,7 +109,7 @@ func TestAddToBeginning(t *testing.T) {
 	node1.Next = &node2
 	node2.Prev = &node1
 
-	list := list{&node1, &node2}
+	list := list{&node1, &node2, 2}
 
 	t.Log("Expecting first node to node 3, pointing Next to node 1 and previous to nil.")
 	t.Logf("Initial list: %+v", list)
@@ -119,6 +131,10 @@ func TestAddToBeginning(t *testing.T) {
 	if list.Head.Prev != nil {
 		t.Errorf("Error: Head is not pointin previous to nil.")
 	}
+
+	if list.Length != 3 {
+		t.Errorf("Error: Length did not increase from 2 to 3")
+	}
 }
 
 func TestFindNodeByValue(t *testing.T) {
@@ -131,7 +147,7 @@ func TestFindNodeByValue(t *testing.T) {
 	node2.Next = &node3
 	node3.Prev = &node2
 
-	list := list{&node1, &node3}
+	list := list{&node1, &node3, 3}
 
 	foundNode, err := list.findNodeByValue(2)
 
@@ -158,7 +174,7 @@ func TestFindNodeByValueNoNodePresent(t *testing.T) {
 	node2.Next = &node3
 	node3.Prev = &node2
 
-	list := list{&node1, &node3}
+	list := list{&node1, &node3, 3}
 
 	t.Log("Expecting to find no matching node.")
 
@@ -173,5 +189,149 @@ func TestFindNodeByValueNoNodePresent(t *testing.T) {
 
 	if foundNode != nil {
 		t.Errorf("Error: foundNode is not nil even though no node with that value is present in the list.")
+	}
+}
+
+func TestRemoveFromEnd(t *testing.T) {
+	node1 := node{1, nil, nil}
+	node2 := node{2, nil, nil}
+	node3 := node{3, nil, nil}
+
+	node1.Next = &node2
+	node2.Prev = &node1
+	node2.Next = &node3
+	node3.Prev = &node2
+
+	list := list{&node1, &node3, 3}
+
+	t.Log("Expecting the list to be shortened by one node, with the final node being node 2.")
+	t.Logf("Initial tail: %+v", list.Tail)
+
+	list.removeFromEnd()
+
+	t.Logf("Final tail: %+v", list.Tail)
+
+	if list.Tail != &node2 {
+		t.Errorf("Error: Tail is not equal to node 2.")
+	}
+
+	if list.Tail.Next != nil {
+		t.Errorf("Error: Tail is not pointing next to nil.")
+	}
+
+	if list.Length != 2 {
+		t.Errorf("Error: Length did not decrease from 3 to 2")
+	}
+}
+
+func TestRemoveFromEndNoTail(t *testing.T) {
+	list := list{nil, nil, 0}
+
+	t.Log("Expecting to see an error when trying to removeFromEnd of an empty list.")
+
+	err := list.removeFromEnd()
+
+	t.Logf("Error produced: %+v", err)
+
+	if err == nil {
+		t.Errorf("Error: No error produced when running removeFromEnd")
+	}
+
+	if list.Tail != nil {
+		t.Errorf("Error: Tail is not nil.")
+	}
+
+	if list.Length != 0 {
+		t.Errorf("Error: Length is not 0.")
+	}
+}
+
+func TestRemoveFromBeginning(t *testing.T) {
+	node1 := node{1, nil, nil}
+	node2 := node{2, nil, nil}
+	node3 := node{3, nil, nil}
+
+	node1.Next = &node2
+	node2.Prev = &node1
+	node2.Next = &node3
+	node3.Prev = &node2
+
+	list := list{&node1, &node3, 3}
+
+	t.Log("Expecting the list to be shortened by one node, with the first node being node 2.")
+
+	err := list.removeFromBeginning()
+
+	t.Logf("Current head: %+v", list.Head)
+	t.Logf("Node 2: %+v", node2)
+
+	if err != nil {
+		t.Error("Error produced when running removeFromBeginning.")
+		t.Logf("Error: %+v", err)
+	}
+
+	if list.Head != &node2 {
+		t.Error("Error: Head is not equal to node 2")
+	}
+
+	if list.Head.Prev != nil {
+		t.Error("Error: Head is not pointing previous to nil")
+	}
+
+	if list.Length != 2 {
+		t.Errorf("Error: Length did not decrease from 3 to 2")
+	}
+}
+
+func TestRemoveFromBeginningNoHead(t *testing.T) {
+	list := list{nil, nil, 0}
+
+	t.Log("Expecting to see an error when trying to remove from the front of an empty list.")
+
+	err := list.removeFromBeginning()
+
+	if err == nil {
+		t.Errorf("Error: err is nil after removeFromBeginning is run.")
+	}
+
+	if list.Head != nil {
+		t.Errorf("Error: Head is not nil.")
+	}
+
+	if list.Length != 0 {
+		t.Errorf("Error: Length is not 0.")
+	}
+}
+
+func TestIsEmptyOnEmptyList(t *testing.T) {
+	list := list{nil, nil, 0}
+
+	t.Log("Expecting for the return from isEmpty to be true.")
+
+	isListEmpty := list.isEmpty()
+
+	if isListEmpty != true {
+		t.Errorf("Error: isListEmpty is not true.")
+	}
+}
+
+func TestIsEmptyOnNonEmptyList(t *testing.T) {
+	node1 := node{1, nil, nil}
+	node2 := node{2, nil, nil}
+	node3 := node{3, nil, nil}
+
+	node1.Next = &node2
+	node2.Prev = &node1
+	node2.Next = &node3
+	node3.Prev = &node2
+
+	list := list{&node1, &node3, 3}
+
+	t.Log("Expecting for the return from isEmpty to be false.")
+
+	isListEmpty := list.isEmpty()
+
+	if isListEmpty != false {
+		t.Errorf("Error: isListEmpty is true.")
 	}
 }
--- goProjects/data_structures/linked_list/linked_lists.go
+++ goProjects/data_structures/linked_list/linked_lists.go
@@ -4,15 +4,23 @@ package main
 //head and tail are node objects
 //node struct contain value, pointer to prev node, pointer to next node
 //you should be able to
-//add a node (to the end)
+//add a node (to the end) done
+//add a node (to the beginning) done
+//remove a node (from the end) done
+//remove a node (from the beginning) done
+//determine is list is empty done
+//determine length of list done
+//find a node in a list done
 //remove a node (with value)
 //remove a node (given the node that comes before it or after it)
-//find a node in a list
+//add a node after a given node
+//add a node before a given node
+//remove a node after a given node
+//remove a node before a given node
 //determine if list is empty
 
 import (
 	"errors"
-	"fmt"
 )
 
 type node struct {
@@ -22,18 +30,31 @@ type node struct {
 }
 
 type list struct {
-	Head *node
-	Tail *node
+	Head   *node
+	Tail   *node
+	Length int
+}
+
+func (list *list) isEmpty() bool {
+	var listIsEmpty bool
+
+	if list.Length == 0 && list.Head == nil && list.Tail == nil {
+		listIsEmpty = true
+	}
+
+	return listIsEmpty
 }
 
 func (list *list) addToEnd(newNode *node) {
 	if list.Head == nil {
 		list.Head = newNode
 		list.Tail = newNode
+		list.Length++
 	} else {
 		list.Tail.Next = newNode
 		newNode.Prev = list.Tail
 		list.Tail = newNode
+		list.Length++
 	}
 }
 
@@ -41,11 +62,47 @@ func (list *list) addToBeginning(newNode *node) {
 	if list.Head == nil {
 		list.Head = newNode
 		list.Tail = newNode
+		list.Length++
 	} else {
 		newNode.Next = list.Head
 		list.Head.Prev = newNode
 		list.Head = newNode
+		list.Length++
 	}
+}
+
+func (list *list) removeFromEnd() error {
+	var err error
+
+	if list.Tail == nil {
+		err = errors.New("Error in removeFromEnd, tail is nil.")
+		return err
+	} else {
+		list.Tail.Prev.Next = nil
+		list.Tail = list.Tail.Prev
+		if list.Length != 0 {
+			list.Length--
+		}
+	}
+
+	return err
+}
+
+func (list *list) removeFromBeginning() error {
+	var err error
+
+	if list.Head == nil {
+		err = errors.New("Error in removeFromBeginning, head is nil.")
+		return err
+	} else {
+		list.Head.Next.Prev = nil
+		list.Head = list.Head.Next
+		if list.Length != 0 {
+			list.Length--
+		}
+	}
+
+	return err
 }
 
 func (list *list) findNodeByValue(nodeValue int) (*node, error) {
@@ -63,8 +120,4 @@ func (list *list) findNodeByValue(nodeValue int) (*node, error) {
 	}
 
 	return currentNode, err
-}
-
-func main() {
-	fmt.Println("linked_list.go ran")
 }
