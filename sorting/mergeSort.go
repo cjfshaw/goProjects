160dce6f562aeb5d3030d0696a867fa6fc47be2d
cjfshaw <cjfshaw@gmail.com>
2016-03-22 00:13:38 -0700

    added merging and testing around it

--- sorting/mergeSort.go
+++ sorting/mergeSort.go
@@ -51,6 +51,52 @@ func splitArray(input []int) ([]int, []int) {
 	return left, right
 }
 
+func sortArrayOfTwo(input []int) []int {
+	if input[0] > input[1] {
+		temp := input[1]
+		input[1] = input[0]
+		input[0] = temp
+		return input
+	} else {
+		return input
+	}
+}
+
+func sortSingleArray(input []int) ([]int, error) {
+	var err error
+
+	/*if len(input) > 2 {
+		err = fmt.Errorf("Error: Length of array in sortSingleArray is greater than two.\nLength: %v", len(input))
+		return input, err
+	}*/
+
+	if len(input) == 1 {
+		return input, err
+	} else {
+		sorted := sortArrayOfTwo(input)
+		return sorted, err
+	}
+}
+
+func mergeTwoArrays(left []int, right []int) []int {
+	newArray := make([]int, len(left)+len(right))
+
+	lowestLeft := 0
+	lowestRight := 0
+
+	for i := 0; i < len(newArray); i++ {
+		if lowestLeft < len(left) && left[lowestLeft] <= right[lowestRight] {
+			newArray[i] = left[lowestLeft]
+			lowestLeft++
+		} else if lowestRight < len(right) {
+			newArray[i] = right[lowestRight]
+			lowestRight++
+		}
+	}
+
+	return newArray
+}
+
 /*func mergeSort(input []int) []int {
 	left, right := splitArray(input)
 
--- sorting/mergeSort_test.go
+++ sorting/mergeSort_test.go
@@ -3,15 +3,25 @@ package mergesort
 import "testing"
 
 type testArrays struct {
-	EvenArray []int
-	OddArray  []int
+	EvenArray   []int
+	OddArray    []int
+	One         []int
+	TwoUnsorted []int
+	TwoSorted   []int
+	EvenSorted  []int
+	OddSorted   []int
 }
 
 func makeArrays() testArrays {
 	Even := []int{7, 1, 8, 3}
 	Odd := []int{7, 1, 4, 8, 3}
+	One := []int{1}
+	TwoUnsorted := []int{7, 1}
+	TwoSorted := []int{1, 7}
+	EvenSorted := []int{1, 7}
+	OddSorted := []int{3, 4, 8}
 
-	arrays := testArrays{EvenArray: Even, OddArray: Odd}
+	arrays := testArrays{EvenArray: Even, OddArray: Odd, One: One, TwoUnsorted: TwoUnsorted, TwoSorted: TwoSorted, EvenSorted: EvenSorted, OddSorted: OddSorted}
 
 	return arrays
 }
@@ -120,5 +130,86 @@ func TestSplitArraysOdd(t *testing.T) {
 
 	if right[0] != 4 && right[1] != 8 && right[2] != 3 {
 		t.Errorf("Error: Values of right array are incorrect.\nArray: %v", right)
+	}
+}
+
+func TestSortArrayOfTwoOnSorted(t *testing.T) {
+	arrays := makeArrays()
+
+	t.Log("Testing whether sorting of a single array works when the array has two (sorted) values.")
+
+	sorted := sortArrayOfTwo(arrays.TwoSorted)
+
+	if sorted[0] != arrays.TwoSorted[0] && sorted[1] != arrays.TwoSorted[1] {
+		t.Errorf("Error: Arrays are not equal.\nsorted: %v\noriginal: %v", sorted, arrays.TwoSorted)
+	}
+
+	if len(sorted) != len(arrays.TwoSorted) {
+		t.Errorf("Error: Sorted array's length is different from original array's length.\nLength sorted: %v\nLength original: %v", len(sorted), len(arrays.TwoSorted))
+	}
+}
+
+func TestSortArrayOfTwoOnUnsorted(t *testing.T) {
+	arrays := makeArrays()
+
+	t.Log("Testing whether sorting of a single array works when the array has two (sorted) values.")
+
+	sorted := sortArrayOfTwo(arrays.TwoUnsorted)
+
+	if sorted[0] != arrays.TwoSorted[0] && sorted[1] != arrays.TwoUnsorted[1] {
+		t.Errorf("Error: Arrays are not equal.\nsorted: %v\noriginal: %v", sorted, arrays.TwoUnsorted)
+	}
+
+	if len(sorted) != len(arrays.TwoUnsorted) {
+		t.Errorf("Error: Sorted array's length is different from original array's length.\nLength sorted: %v\nLength original: %v", len(sorted), len(arrays.TwoUnsorted))
+	}
+}
+
+func TestSortSingleArrayOnOne(t *testing.T) {
+	arrays := makeArrays()
+
+	t.Log("Testing whether sorting of a single array works when the array has one value.")
+
+	sorted, err := sortSingleArray(arrays.One)
+
+	t.Log("Array has been sorted.")
+
+	if sorted[0] != arrays.One[0] {
+		t.Errorf("Error: Sorted array is not equal to arrays.One.\nSorted: %v\narrays.One: %v", sorted, arrays.One)
+	}
+
+	if len(sorted) != 1 {
+		t.Errorf("Error: Length of sorted array is not one.\nLengthL %v", len(sorted))
+	}
+
+	if err != nil {
+		t.Errorf("Error: err is not nil.\n%v", err)
+	}
+}
+
+func TestSortingArrayOnMultiple(t *testing.T) {
+	arrays := makeArrays()
+
+	t.Log("Testing whether sorting of a single array works when the array has multiple values.")
+	t.Logf("Starting Arrays:\narrays.One: %v\narrays.TwoSorted: %v", arrays.One, arrays.TwoSorted)
+
+	newArray := mergeTwoArrays(arrays.One, arrays.TwoSorted)
+	t.Logf("Merged Array: %v", newArray)
+
+	for i := 1; i < len(newArray); i++ {
+		if newArray[i] < newArray[i-1] {
+			t.Errorf("Error: Array is not sorted.\nArray: %v", newArray)
+		}
+	}
+
+	t.Logf("Starting Arrays:\narrays.EvenSorted: %v\narrays.OddSorted: %v", arrays.EvenSorted, arrays.OddSorted)
+
+	newArray2 := mergeTwoArrays(arrays.EvenSorted, arrays.OddSorted)
+	t.Logf("Merged Array: %v", newArray2)
+
+	for i := 1; i < len(newArray2); i++ {
+		if newArray2[i] < newArray2[i-1] {
+			t.Errorf("Error: Array is not sorted.\nArray: %v", newArray2)
+		}
 	}
 }
