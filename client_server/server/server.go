8470688ebb7a5ca55bf5f843a99a46919983f627
cjfshaw <cjfshaw@gmail.com>
2016-03-26 02:17:26 -0700

    got client server working, all about directory structure when it comes to golang

---
+++ client_server/server/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func startListener() (net.Listener, error) {
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		return listener, err
+	}
+
+	return listener, err
+}
+
+func main() {
+	var closeConnection bool
+	var message string
+
+	listener, err := startListener()
+
+	if err != nil {
+		fmt.Println("Error occurred while trying to start the listener.")
+		fmt.Println(err)
+	}
+
+	connection, err := listener.Accept()
+
+	for closeConnection == false {
+		scanner := bufio.NewScanner(connection)
+		scanner.Scan()
+		message = scanner.Text()
+
+		if message == "close" {
+			closeConnection = true
+		}
+
+		if message != "" {
+			fmt.Println("Message received:")
+			fmt.Println(message)
+		}
+	}
+
+	err = listener.Close()
+
+	if err != nil {
+		fmt.Println("Error closing the connection.")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Listener closed, server shut down!.")
+	}
+}
